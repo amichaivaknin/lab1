8f5ebd19da1dd7a3b23a40457fc6eb5535e5c346
AmichaiVak <amiko565@gmail.com>
2016-06-17 09:13:30 +0300

    Add files via upload

---
+++ lab1_2/calc.cs
@@ -0,0 +1,12 @@
+class Calc 
+{ 
+    static int Add(int a, int b) 
+        { 
+        return a + b; 
+        } 
+		
+	static int Subtract(int a, int b) 
+        { 
+        return a - b; 
+        } 
+}
